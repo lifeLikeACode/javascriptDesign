879b84c4e867f9a853ee0f9834224827d9c40128
xujian <562681041@qq.com>
2018-08-03 17:54:31 +0800

    add babel-loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert(1)
+class Person {
+  constructor(name){
+    this.name = name
+  }
+  getName (){
+    return this.name
+  }
+  setName(name){
+    this.name = name
+  }
+}
+const p = new Person('xj')
+alert(p.getName())
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry:{
     index:'./src/index.js'
@@ -5,6 +7,24 @@ module.exports = {
   output:{
     path:__dirname,
     filename: './release/bundle.js'
+  },
+  module:{
+    rules:[{
+      test: /\.js?$/,
+      exclude:/(node_modules)/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer:{
+    contentBase: path.join(__dirname, './release'), //根目录
+    open: true, //自动打开浏览器
+    port: 9000,
+    
   },
   mode:'development'
 }
